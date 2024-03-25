c752ff96c5e0e8b729e5de0f4afa8af997ec93ce
jheferson69 <alfaomegajhismar@gmail.com>
2024-03-25 07:17:25 -0400

    Nave enemiga ya dispara

--- Source/GALAGA_USFX_LAB01/NaveEnemigaCaza.cpp
+++ Source/GALAGA_USFX_LAB01/NaveEnemigaCaza.cpp
@@ -2,10 +2,41 @@
 
 
 #include "NaveEnemigaCaza.h"
+#include "GALAGA_USFX_LAB01Projectile.h"
 
 ANaveEnemigaCaza::ANaveEnemigaCaza()
 {
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_TriPyramid.Shape_TriPyramid'"));
 	mallaNaveEnemiga->SetStaticMesh(ShipMesh.Object);
+	TimeSinceLastShot = 0.0f; // Inicializa el tiempo desde el último disparo
+	ShotInterval = 1.0f; // Intervalo de tiempo entre disparos
 }
+void ANaveEnemigaCaza::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	// Incrementar el tiempo transcurrido desde el último disparo
+	TimeSinceLastShot += DeltaTime;
 
+	// Verificar si ha pasado el tiempo suficiente para disparar
+	if (TimeSinceLastShot >= ShotInterval)
+	{
+		// Reiniciar el contador de tiempo
+		TimeSinceLastShot = 0.0f;
+
+		// Llamar a la función para disparar
+		Disparar();
+	}
+}
+void ANaveEnemigaCaza::Disparar()
+{
+	FVector PosicionActualNaveEnemigaCaza = GetActorLocation();
+	FRotator RotacionActualNaveEnemigaCaza = GetActorRotation();
+
+	FVector DireccionDisparo = RotacionActualNaveEnemigaCaza.Vector();
+	PosicionActualNaveEnemigaCaza += DireccionDisparo * 100;
+	UWorld* const World = GetWorld();
+	if (World)
+	{
+		World->SpawnActor<AGALAGA_USFX_LAB01Projectile>(PosicionActualNaveEnemigaCaza, RotacionActualNaveEnemigaCaza);
+	}
+}
--- Source/GALAGA_USFX_LAB01/NaveEnemigaCaza.h
+++ Source/GALAGA_USFX_LAB01/NaveEnemigaCaza.h
@@ -15,9 +15,14 @@ class GALAGA_USFX_LAB01_API ANaveEnemigaCaza : public ANaveEnemiga
 	GENERATED_BODY()
 private:
 	int cantidadBombas;
+	float TimeSinceLastShot;
+	float ShotInterval;
 public:
 	ANaveEnemigaCaza();
 	FORCEINLINE int GetCantidadBombas() const { return cantidadBombas; }
 	FORCEINLINE void SetCantidadBombas(int _cantidadBombas) { cantidadBombas = _cantidadBombas; }
-
+public:
+	 virtual void Disparar();
+public: 
+	virtual void Tick(float DeltaTime) override;
 };
